e99217e7e264cd4ad536e64708e698fb24949395
Alter <tuuchan0420@gmail.com>
2024-02-19 18:36:00 +0900

    UserCountsの実装及びDB準備

--- server/api/qapi/counter.go
+++ server/api/qapi/counter.go
@@ -6,14 +6,14 @@ import (
 )
 
 // traQAPI(/users/{userid}/stats)を叩きまくる *高負荷かつ長時間
-func (q *QapiHandler) GetUserPostCount() {
+func (q *QapiHandler) GetUserPostCount() *[]model.QUserMessages {
 
 	//ユーザリスト取得
 	v, _, err := q.client.UserApi.GetUsers(q.auth).Execute()
 
 	if err != nil {
 		log.Println("Internal error-Qapi error:", err.Error())
-		return
+		return nil
 	}
 
 	log.Println("Userlistavailable:", len(v))
@@ -30,7 +30,7 @@ func (q *QapiHandler) GetUserPostCount() {
 		userStats, _, err := q.client.UserApi.GetUserStats(q.auth, user.Id).Execute()
 		if err != nil {
 			log.Println("Internal error-Qapi error:", err.Error())
-			return
+			return nil
 		}
 		var message model.QUserMessages
 		message.User = user
@@ -40,8 +40,19 @@ func (q *QapiHandler) GetUserPostCount() {
 		log.Println("traQing:", i, "mescount:", message.TotalMessageCount)
 	}
 
+	return &userMessages
+
 }
 
-func (q *QapiHandler) GetUserDetails() *model.UserDetailWithMessageCount{
+func (q *QapiHandler) GetUserDetails(userMessages *[]model.QUserMessages) *[]model.UserDetailWithMessageCount {
+	userdetails := []model.UserDetailWithMessageCount{}
+	for _, userMessage := range *userMessages {
+		user:=userMessage.User
+		userdetails = append(userdetails,model.UserDetailWithMessageCount{
+			Id: user.Id,
+			TotalMessageCount: userMessage.TotalMessageCount,
+		})
+
+	}
 	return nil
 }
