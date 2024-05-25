c36164ebd18845cacce8e18838f16b704ff077eb
Alter <tuuchan0420@gmail.com>
2024-05-25 14:58:41 +0900

    gtactiveへの送信設定(テスト用に1)

--- server/handler/channelflow.go
+++ server/handler/channelflow.go
@@ -19,6 +19,7 @@ func (h *Handler) RecentMessageCollector(since time.Duration) ([]RecentMessageCo
 
 	if len(messagecounts) != 0 {
 		h.SendingDMs(messagecounts)
+		h.SendingGTActive(messagecounts)
 		err = h.DeleteRecentMessageDB()
 		if err != nil {
 			return messagecounts, err
@@ -26,7 +27,7 @@ func (h *Handler) RecentMessageCollector(since time.Duration) ([]RecentMessageCo
 		}
 	}
 	return messagecounts, nil
-	
+
 }
 
 // 25h以前のメッセージ履歴の消去
@@ -72,10 +73,44 @@ func (h *Handler) SendingDMs(messagecounts []RecentMessageCountbyChannel) {
 			}
 		}
 		if content != "" {
-			contentsend := "## 直近15分の流速案内\n|チャンネル|投稿数/15分|\n|---|---|\n" + content+
-			"\n現在の基準流速:"+strconv.Itoa(s.NotifyFlowAmount)+" `@BOT_traQer \\dm\\enroll {値}`で変更可"
+			contentsend := "## 直近15分の流速案内\n|チャンネル|投稿数/15分|\n|---|---|\n" + content +
+				"\n現在の基準流速:" + strconv.Itoa(s.NotifyFlowAmount) + " `@BOT_traQer \\dm\\enroll {値}`で変更可"
 			h.b.BotDM(s.Userid, contentsend)
 		}
+	}
+
+}
+
+// g/t/active チャンネルへの配信
+
+func (h *Handler) SendingGTActive(messagecounts []RecentMessageCountbyChannel) {
+	// 流れのいいチャンネルを通知
+	// 購読者ごとに追加していきたい
+	// 購読者の取得(基準メッセ数が多い順)->メッセージを伸ばしていく感じ->内容ができたら配信
+
+	i := 0                                             //messagecountsのどこまで消化したかのイテレーション
+	notifycount := 1                                   //通知数の設定
+	gtactive := "1e247400-962f-4cf9-8def-2051f815cd78" //送信先
+	content := ""
+
+	for messagecounts[i].Count >= notifycount {
+		channelname, err := h.GetChannelNameWithParents(messagecounts[i].Channelid, "")
+		if err != nil {
+			log.Println("Error getting channelname:", err.Error())
+			return
+		}
+		// とりあえずは15分のみ対応
+		content += "|**[#" + channelname + "]" +
+			"(https://q.trap.jp/channels/" + channelname + ")**|**" +
+			strconv.Itoa(messagecounts[i].Count) + "**|\n"
+		i++
+		if i == len(messagecounts) {
+			break
+		}
+	}
+	if content != "" {
+		contentsend := "## 直近15分の流速案内\n|チャンネル|投稿数/15分|\n|---|---|\n" + content
+		h.b.BotSimplePost(gtactive, contentsend)
 	}
 
 }
